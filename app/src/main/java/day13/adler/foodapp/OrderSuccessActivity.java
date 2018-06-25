9f930074302cc013729cec9dbcb468f75d69f536
HAITTSE62631\USER <haitt19971212@gmail.com>
2018-06-25 11:27:18 +0900

    newest
    ver 2.1

--- app/src/main/java/day13/adler/foodapp/OrderSuccessActivity.java
+++ app/src/main/java/day13/adler/foodapp/OrderSuccessActivity.java
@@ -4,6 +4,11 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class OrderSuccessActivity extends AppCompatActivity {
 
@@ -11,6 +16,19 @@ public class OrderSuccessActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order_success);
+
+
+        Spinner spinner = (Spinner) findViewById(R.id.Spinner01);
+        ArrayAdapter<String> adapter;
+        List<String> list;
+
+        list = new ArrayList<>();
+        list.add("Thịt Kho Ba Rọi");
+        list.add("Cá Kho Tộ");
+        adapter = new ArrayAdapter<String>(this, R.layout.spinner_item,list);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
     }
 
     public void backToLast(View view) {super.onBackPressed();
