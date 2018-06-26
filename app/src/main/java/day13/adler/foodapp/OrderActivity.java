d5598004a6ed11e7705c1fc1e1cbc748a025a060
HAITTSE62631\USER <haitt19971212@gmail.com>
2018-06-26 23:41:06 +0900

    foodapp
    layout v4

--- app/src/main/java/day13/adler/foodapp/OrderActivity.java
+++ app/src/main/java/day13/adler/foodapp/OrderActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.RadioButton;
 import android.widget.Spinner;
 
 import java.util.ArrayList;
@@ -13,21 +14,26 @@ import java.util.List;
 
 public class OrderActivity extends AppCompatActivity {
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order);
 
-        Spinner spinner = (Spinner) findViewById(R.id.Spinner01);
-        ArrayAdapter<String> adapter;
-        List<String> list;
+        RadioButton rd1 = (RadioButton)findViewById(R.id.rdb1);
+        rd1.setChecked(true);
 
-        list = new ArrayList<>();
-        list.add("Cooked (20$ / each)");
-        list.add("Ingredient Only (8$ / each)");
-        adapter = new ArrayAdapter<String>(this, R.layout.spinner_item,list);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(adapter);
+//        Spinner spinner = (Spinner) findViewById(R.id.Spinner01);
+//        ArrayAdapter<String> adapter;
+//        List<String> list;
+
+//        list = new ArrayList<>();
+//        list.add("Cooked (20$ / each)");
+//        list.add("Ingredient Only (8$ / each)");
+//        adapter = new ArrayAdapter<String>(this, R.layout.spinner_item,list);
+//        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+//        spinner.setAdapter(adapter);
 
 
 
@@ -42,17 +48,28 @@ public class OrderActivity extends AppCompatActivity {
 
     public void goToTrackingOrder(View view) {
 
+        RadioButton rd1 = (RadioButton)findViewById(R.id.rdb1);
+        RadioButton rd2 = (RadioButton)findViewById(R.id.rdb2);
 
-        Spinner spinner = (Spinner) findViewById(R.id.Spinner01);
-        String text = spinner.getSelectedItem().toString();
-        if (text == "Cooked (20$ / each)"){
+        if (rd1.isChecked()){
             Intent intent = new Intent(this, OrderSuccessActivity.class);
             startActivity(intent);
         }
-        if (text == "Ingredient Only (8$ / each)"){
+        if (rd2.isChecked()){
             Intent intent = new Intent(this, OrderSuccess2Activity.class);
             startActivity(intent);
         }
+
+//        Spinner spinner = (Spinner) findViewById(R.id.Spinner01);
+//        String text = spinner.getSelectedItem().toString();
+//        if (text == "Cooked (20$ / each)"){
+//            Intent intent = new Intent(this, OrderSuccessActivity.class);
+//            startActivity(intent);
+//        }
+//        if (text == "Ingredient Only (8$ / each)"){
+//            Intent intent = new Intent(this, OrderSuccess2Activity.class);
+//            startActivity(intent);
+//        }
 
     }
 }
